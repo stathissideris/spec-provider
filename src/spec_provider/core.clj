50829d6969c38972c5e8ea8f9368306795349923
Stathis Sideris <stathis@pixelated-noise.com>
2016-06-13 01:53:51 +0300

    Add nested address map to example schema

--- src/spec_provider/core.clj
+++ src/spec_provider/core.clj
@@ -92,15 +92,25 @@
              stats)
             [(summarize-keys stats)])))
 
-(s/def ::id (s/or :numeric (s/and integer? pos?) :string string?))
+(s/def ::id (s/or :numeric pos-long? :string string?))
 (s/def ::first-name string?)
 (s/def ::surname string?)
 (s/def ::k keyword?)
 (s/def ::age (s/and integer? pos?))
 (s/def ::role #{:programmer :designer})
 (s/def ::phone-number string?)
+
+(s/def ::street string?)
+(s/def ::city string?)
+(s/def ::country string?)
+(s/def ::street-number pos-long?)
+
+(s/def ::address
+  (s/keys :req-un [::street ::city ::country]
+          :opt-un [::street-number]))
+
 (s/def ::person
-  (s/keys :req-un [::id ::first-name ::surname ::k ::age ::role]
+  (s/keys :req-un [::id ::first-name ::surname ::k ::age ::role ::address]
           :opt-un [::phone-number]))
 
 ;;(s/form (s/or :numeric (s/and integer? pos?) :string string?))
