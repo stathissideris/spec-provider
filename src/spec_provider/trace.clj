1d93126b270c5d4b578fea6e3798fe5deccc4787
Stathis Sideris <stathis@pixelated-noise.com>
2016-09-21 02:09:48 +0300

    spec for parsing fn args

--- src/spec_provider/trace.clj
+++ src/spec_provider/trace.clj
@@ -1,11 +1,14 @@
 (ns spec-provider.trace
-  (:require [spec-provider.stats :as stats]))
+  (:require [clojure.spec :as s]
+            [clojure.walk :as walk]
+            [spec-provider.stats :as stats]
+            [spec-provider.provider :as provider]))
 
-(defn record-arg-values! [fn-name a args]
-  (swap! a update-in [fn-name :args] #(stats/update-stats % args)))
+(defn- record-arg-values! [fn-name a args]
+  (swap! a update-in [fn-name :args] #(stats/update-stats % args {:positional true})))
 
-(defn record-return-value! [fn-name a val]
-  (swap! a update-in [fn-name :return] #(stats/update-stats % val))
+(defn- record-return-value! [fn-name a val]
+  (swap! a update-in [fn-name :return] #(stats/update-stats % val {}))
   val)
 
 (defn- arg-name [arg]
@@ -13,16 +16,78 @@
         (map? arg) (:as arg)
         (vector? arg) (last arg)))
 
+(defn- vector-has-as? [v]
+  (= :as (first (take-last 2 v))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(s/def ::as (s/cat :as #{:as}
+                   :name symbol?))
+
+(s/def ::simple-arg symbol?)
+(s/def ::vector-destr (s/and vector?
+                             (s/cat :args    (s/* ::arg)
+                                    :varargs (s/? (s/cat :amp #{'&}
+                                                         :name symbol?))
+                                    :as      (s/? ::as))))
+
+(s/def ::map-destr (s/or :keys-destr ::keys-destr
+                         :syms-destr ::syms-destr
+                         :strs-destr ::strs-destr
+                         :explicit-destr ::explicit-destr))
+
+(s/def ::map-as symbol?)
+
+(s/def ::or (s/map-of any? any?))
+
+(s/def ::keys (s/coll-of symbol? :kind vector?))
+(s/def ::keys-destr (s/keys :req-un [::keys] :opt-un [::map-as ::or]))
+
+(s/def ::syms (s/coll-of symbol? :kind vector?))
+(s/def ::syms-destr (s/keys :req-un [::syms] :opt-un [::map-as ::or]))
+
+(s/def ::strs (s/coll-of symbol? :kind vector?))
+(s/def ::strs-destr (s/keys :req-un [::strs] :opt-un [::map-as ::or]))
+
+(s/def ::explicit-destr (s/and
+                         (s/keys :opt-un [::map-as ::or] :conform-keys true)
+                         (s/map-of (s/or :name symbol?
+                                         :nested-destr ::arg)
+                                   (s/or :destr-key any?) ;;or with single case just to get tagged value
+                                   :conform-keys true)))
+
+(s/def ::arg (s/and (complement #{'&})
+                    (s/or :name ::simple-arg
+                          :map-destr ::map-destr
+                          :vector-destr ::vector-destr)))
+
+(s/def ::args (s/coll-of ::arg :kind vector?))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defn- extract-arg-names [args]
+  (map
+   (fn [arg]
+     (walk/postwalk
+      (fn [x]
+        (cond
+          (symbol? x) {:type :simple :name x}
+          (vector? x) {:type :vector :name (last x) :elements (if (vector-has-as? x)
+                                                                (drop-last 2 x) x)}
+          (map? x)    {:type :map :name (:as x) :keys (keys x)}))
+      arg))
+   args))
+
 (defn record-arg-names! [fn-name a arg-names]
   (swap! a assoc-in [fn-name :arg-names] arg-names))
 
-(defn- fix-arg [idx arg]
+(defn- fix-arg [idx arg] ;;TODO bug: assigned names may already exist
   (cond (map? arg)
         (if (:as arg)
           arg
           (assoc arg :as (symbol (str "map" idx))))
         (vector? arg)
-        (if (= :as (first (take-last 2 arg)))
+        (if (vector-has-as? arg)
           arg
           (into arg [:as (symbol (str "vec" idx))]))
         :else
@@ -34,12 +99,12 @@
 (defn- instrument-body [fn-name a body]
   (let [args (fix-args (first body))]
     `(~args
-      (record-arg-names! ~fn-name ~a (quote ~(mapv arg-name args)))
+      (record-arg-names! ~fn-name ~a (quote ~(extract-arg-names args)))
       (record-arg-values! ~fn-name ~a ~(mapv arg-name args))
       ~@(drop 1 (butlast body))
       (record-return-value! ~fn-name ~a ~(last body)))))
 
-(defmacro instrument [a defn-code]
+(defmacro instrument [trace-atom defn-code]
   (assert (= 'defn (first defn-code)))
   (let [fn-name  (second defn-code)
         start    (if (string? (nth defn-code 2))
@@ -53,14 +118,46 @@
                    [rest]
                    (vec rest))
         atom-sym (gensym)]
-    `(let [~atom-sym a]
+    `(let [~atom-sym ~trace-atom]
        (defn ~fn-name ~doc
          ~@(map #(instrument-body (str *ns* "/" fn-name) atom-sym %) bodies)))))
 
-(def a (atom {}))
+(defn- set-cat-names [cat-spec names]
+  (let [parts (partition 2 (drop 1 (second cat-spec)))]
+    (list
+     `s/spec ;;TODO assumes that all s/cats are wrapped in an s/spec
+     (concat (list `s/cat)
+             (mapcat (fn [name [_ spec]] [name spec]) names parts)))))
 
-(instrument a
-  (defn foo "doc" [a b [v1 v2] c d {:keys [foo bar]}]
-    (swap! (atom []) conj 1)
-    (swap! (atom []) conj 2)
-    (* d (+ a b c))))
+(defn fn-spec [trace-atom fn-name]
+  (let [fn-name      (str fn-name)
+        stats        (get @trace-atom fn-name)
+        arg-names    (map keyword (:arg-names stats))
+        arg-stats    (:args stats)
+        return-stats (:return stats)
+        arg-specs    (set-cat-names
+                      (-> (provider/summarize-stats arg-stats fn-name)
+                          first (nth 2))
+                      arg-names)
+        return-spec (-> (provider/summarize-stats return-stats fn-name)
+                        first (nth 2))]
+    (list `s/fdef (symbol fn-name)
+          :args arg-specs
+          :ret return-spec)))
+
+(defn pprint-fn-spec [trace-atom fn-name domain-ns clojure-spec-ns]
+  (provider/pprint-specs [(fn-spec trace-atom fn-name)] domain-ns clojure-spec-ns))
+
+(defonce a (atom {}))
+
+(comment
+ (instrument a
+             (defn foo "doc" [a b [v1 v2] c d {:keys [foo bar]}]
+               (swap! (atom []) conj 1)
+               (swap! (atom []) conj 2)
+               (* d (+ a b c)))))
+
+(comment
+  (foo 1 2 [3 4] 5 6 {:foo 7 :bar 8})
+  (pprint-fn-spec a 'spec-provider.trace/foo)
+  )
