03e5c5931a2233ad3bf3ceeec3e1509e15eab3e3
Stathis Sideris <stathis@pixelated-noise.com>
2016-11-20 15:10:03 +0200

    WIP improve capturing of arg names

--- test/spec_provider/provider_test.clj
+++ test/spec_provider/provider_test.clj
@@ -9,3 +9,11 @@
   (is (infer-specs (gen/sample (s/gen integer?) 1000) :foo/int))
   (is (infer-specs (gen/sample (s/gen (s/coll-of integer?)) 1000) :foo/coll-of-ints))
   (is (infer-specs (gen/sample (s/gen ::person/person) 100) :foo/person)))
+
+(deftest pprint-specs-test
+  (let [specs '[(clojure.spec/def :person/id (clojure.spec/or :numeric pos-int? :string string?))
+                (clojure.spec/def :person/codes (clojure.spec/coll-of keyword? :max-gen 5))
+                (clojure.spec/def :person/first-name string?)
+                (clojure.spec/def :person/surname string?)
+                (clojure.spec/def :person/k keyword?)]]
+    (pprint-specs specs 'person 's)))
