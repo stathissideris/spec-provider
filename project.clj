353f4a0399f0c8cdc5705b8604a014a7b7e4a86c
Stathis Sideris <stathis@pixelated-noise.com>
2017-04-17 15:26:14 +0300

    Update project.clj url and description

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject spec-provider "0.3.0"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject spec-provider "0.3.1"
+  :description "The equivalent of F#'s type providers but with clojure.spec"
+  :url "https://github.com/stathissideris/spec-provider"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0-alpha15"]]
