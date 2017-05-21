ee195120a341b928d25f420ca94700e44650639e
Stathis Sideris <stathis@pixelated-noise.com>
2017-05-21 14:40:14 +0300

    v0.4.4

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject spec-provider "0.4.3"
+(defproject spec-provider "0.4.4"
   :description "Infer clojure specs from sample data. Inspired by F#'s type providers."
   :url "https://github.com/stathissideris/spec-provider"
   :license {:name "Eclipse Public License"
