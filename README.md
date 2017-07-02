70e1e42a2fc07aafe91aea2afc6f8ff881ae62ee
Stathis Sideris <stathis@pixelated-noise.com>
2017-07-02 16:41:50 +0300

    v0.4.8

--- README.md
+++ README.md
@@ -21,7 +21,7 @@ change, possibly flawed.
 To use this library, add this dependency to your `project.clj` file:
 
 ```
-[spec-provider "0.4.7"]
+[spec-provider "0.4.8"]
 ```
 
 [Version history](https://github.com/stathissideris/spec-provider/blob/master/doc/history.md)
--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject spec-provider "0.4.7"
+(defproject spec-provider "0.4.8"
   :description "Infer clojure specs from sample data. Inspired by F#'s type providers."
   :url "https://github.com/stathissideris/spec-provider"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0-alpha16"]]
+  :dependencies [[org.clojure/clojure "1.9.0-alpha17"]]
   :profiles {:dev {:source-paths ["dev"]
                    :dependencies [[org.clojure/tools.namespace "0.2.11"]
                                   [org.clojure/test.check "0.9.0"]]}})
