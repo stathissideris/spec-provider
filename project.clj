3d26b43cc37679bca84dd3165d45ed136ccf0174
Stathis Sideris <stathis@pixelated-noise.com>
2017-10-13 13:42:21 +0300

    Update Clojure version to latest beta

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/stathissideris/spec-provider"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0-alpha17"]]
+  :dependencies [[org.clojure/clojure "1.9.0-beta2"]]
   :profiles {:dev {:source-paths ["dev"]
                    :dependencies [[org.clojure/tools.namespace "0.2.11"]
                                   [org.clojure/test.check "0.9.0"]]}})
